53a058f0e2aa19996a509ef7744e9cd0b4cf96b9
Rohit Moond <rohitvkmoond@gmail.com>
2023-04-19 15:35:50 +0530

    basic python learn

--- numbers/basics.py
+++ numbers/basics.py
@@ -2,3 +2,7 @@
 # ?why this file has been created
 # !please speakup or i would delete this file
 # todo: need to write functions
+print("Starting my Python journey")
+a =2+5j 
+print(a)
+print (type(a))
