0d6d79bc593651fb6773efec9f16b5bbb28be345
Rohit Moond <rohitvkmoond@gmail.com>
2023-04-19 15:42:21 +0530

    enhancing the knowledge

--- numbers/basics.py
+++ numbers/basics.py
@@ -1,8 +1,12 @@
-# *we need to write code in this file
-# ?why this file has been created
-# !please speakup or i would delete this file
-# todo: need to write functions
 print("Starting my Python journey")
-a =2+5j 
+
+a = 2+5j 
 print(a)
 print (type(a))
+
+#Airthmetic Ops
+a,b = 10, 20
+l = a+b, a-b, a*b, a/b, b-a, b/a
+print(l)
+
+#Relational Ops
