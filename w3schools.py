a14e448c771e8bc53771953a7a2b8a355b45a40e
Rohit Moond <rohitvkmoond@gmail.com>
2023-05-22 16:45:38 +0530

    w3schools coding

---
+++ w3schools.py
@@ -0,0 +1,17 @@
+'''
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+import random
+print(random.randrange(1, 10))
+'''
