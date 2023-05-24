80b2a98cd537d6f1a62b920c206412455df4dd78
Rohit Moond <rohitvkmoond@gmail.com>
2023-05-24 19:20:56 +0530

    .

--- w3schools.py
+++ w3schools.py
@@ -40,13 +40,6 @@ class Car:
   def move(self):
     print("Drive!")
 
-class Boat:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
-  def move(self):
-    print("Sail!")
-
 class Plane:
   def __init__(self, brand, model):
     self.brand = brand
@@ -55,9 +48,8 @@ class Plane:
     print("Fly!")
 
 car1 = Car("Ford", "Mustang")       #Create a Car class
-boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
 plane1 = Plane("Boeing", "747")     #Create a Plane class
 
-for x in (car1, boat1, plane1):
+for x in (car1, plane1):
   x.move()
 '''
