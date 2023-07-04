594208e76a3442446036b0a50de7152950399b3b
Rohit Moond <rohitvkmoond@gmail.com>
2023-07-04 17:38:36 +0530

    Try Except

--- w3schools.py
+++ w3schools.py
@@ -59,4 +59,17 @@ import re
 txt = "The rain in Spain"
 x = re.findall("ai", txt)
 print(x)
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+finally:
+  print("The 'try except' is finished")
+
+x = -1
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
 '''
